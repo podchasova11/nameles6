0ecf72dd0562e44d7b35d1df02b3c0b7ceaac917
Mila <Pod4asova20@gmail.com>
2022-04-14 20:29:10 +0300

    Initial commit

---
+++ src/main/java/lesson06/ClientApp.java
@@ -0,0 +1,28 @@
+package lesson06;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class ClientApp {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        try (Socket socket = new Socket("localhost", 1234)) {
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            while (true) {
+                System.out.println("Введите сообщение: ");
+                String request = scanner.nextLine();
+                dataOutputStream.writeUTF(request);
+                if (request.equals("end")) break;
+                System.out.println("Получили сообщение от сервера: " + dataInputStream.readUTF());
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+}
